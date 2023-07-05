4382549c2ba35638d59e9b50584f959c2068cf02
Fabian Heinrich <127763209+ram-fabian-heinrich@users.noreply.github.com>
2023-07-05 12:18:35 +0200

    Update multi_spf_lookup.ps1

--- multi_spf_lookup.ps1
+++ multi_spf_lookup.ps1
@@ -10,19 +10,26 @@ param (
 $maxSpfRecords = 1
 
 # Process each domain
-$domainData = Get-Content $inputFile | ForEach-Object {
-    $domain = $_
+$domainData = Import-Csv $inputFile -Header 'DomainName' | ForEach-Object {
+    
+    $domain = $_.PSObject.Properties.Value
+    if ($_ -eq 'DomainName' -or $_ -eq '------') {
+        pass
+    }
+
+
     $row = [ordered]@{}
 
     # Lookup TXT records
     try {
-        $txtRecords = Resolve-DnsName $domain -Type TXT
+        $txtRecords = Resolve-DnsName $domain -Type TXT -ErrorAction Stop
+
         # Extract SPF records
         $spfRecords = $txtRecords | Where-Object { $_.Strings -like 'v=spf1*' }
 
         # If there's no SPF record, put an informative placeholder
         if ($null -eq $spfRecords) {
-            $spfRecords = @('No SPF record found')
+            throw @('No SPF record found')
         }
 
         # Update the maximum number of SPF records if necessary
@@ -35,11 +42,13 @@ $domainData = Get-Content $inputFile | ForEach-Object {
         for ($i = 0; $i -lt $spfRecords.Count; $i++) {
             $row["spf-record-$($i+1)"] = $spfRecords[$i].Strings[0]
         }
+        $row['status'] = 'Success'
     } 
     catch {
         # If there was a problem looking up the SPF records, add an informative placeholder
         $row['domain'] = $domain
-        $row['spf-record-1'] = 'Failed to lookup SPF record'
+        $row['spf-record-1'] = ''
+        $row['status'] = 'Failed to lookup SPF record: ' + $_
     }
 
     # Return the row
@@ -47,7 +56,7 @@ $domainData = Get-Content $inputFile | ForEach-Object {
 }
 
 # Create the header for the output file
-$header = "domain"
+$header = "domain;status"
 for ($i = 1; $i -le $maxSpfRecords; $i++) {
     $header += ";spf-record-$i"
 }
